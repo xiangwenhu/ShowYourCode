4cbc97c67e061ba3b8658cffc9eb6580cd5f7b88
TKOFFICE\huxw <huxw@mail.jj.cn>
2021-04-09 15:19:55 +0800

    throttle

---
+++ Functions/throttle.js
@@ -0,0 +1,43 @@
+function isNumer(num){
+    return typeof num === "number";
+}
+
+function isFunction(fn){
+    return typeof  fn === "function";
+}
+
+function isValidNumer(num){
+    return isNumer(num) && num >= 0
+}
+
+// https://github.com/jashkenas/underscore/blob/master/underscore.js#L1099-L1138
+function throttle(fn, wait){
+    if(!isNumer(wait)){
+        throw new TypeError("delay 必须是大于等于0的正数");
+    }
+
+    if(!isFunction(fn)){
+        throw new TypeError("fn必须是一个函数");
+    }
+
+    var last = Date.now();
+    var context = this || null;
+    return function (){
+        var now = Date.now();
+        if(now - last <= wait){
+            return;
+        }  
+        last = now;      
+        return fn.apply(context, arguments);
+    }
+}
+
+
+const log = throttle(function(){
+    console.log("now", Date.now())
+}, 1000);
+
+
+setInterval(()=>{
+    log();
+}, 100)
