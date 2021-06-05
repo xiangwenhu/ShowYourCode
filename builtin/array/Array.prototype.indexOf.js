cf933cb55aa3ad18b0d18993a6b1c2381c3b39fe
TKOFFICE\huxw <huxw@mail.jj.cn>
2021-06-05 15:31:16 +0800

    indexOf

--- builtin/array/Array.prototype.indexOf.js
+++ builtin/array/Array.prototype.indexOf.js
@@ -1,5 +1,33 @@
-Array.prototype.indexOf = function (callback, thisArg) {
 
+var toString = Object.prototype.toString;
+
+function isArray(obj) {
+    return toString.call(obj) === "[object Array]"
+}
+
+Array.prototype.indexOf = function (searchElement, fromIndex) {
+
+    var arr = this;
+    if (!isArray(arr) || arr.length <= 0) {
+        return -1;
+    }
+
+    var item;
+    for (var i = 0; i < arr.length; i++) {
+        item = arr[i];
+
+        if (searchElement === item) {
+            return i;
+        }
+    }
+
+    return -1
 }
 
 
+
+
+var arr = [1, 2, 3];
+
+console.log(arr.indexOf("1"));
+console.log(arr.indexOf(1));
