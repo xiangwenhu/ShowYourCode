a1eac9a8218101049a45c6db171e6750a2ed8c35
TKOFFICE\huxw <huxw@mail.jj.cn>
2021-06-07 14:01:11 +0800

    unshift

--- builtin/array/Array.prototype.unshift.js
+++ builtin/array/Array.prototype.unshift.js
@@ -3,6 +3,57 @@
 var hasOwnProperty = Object.prototype.hasOwnProperty;
 
 
+/**
+ * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+ * https://tc39.es/ecma262/#sec-array.prototype.unshift
+ * @returns 
+ */
+// Array.prototype.unshift = function () {
+
+//     var O = Object(this);
+//     var len = O.length;
+//     var argsObj = arguments;
+//     var argLen = argsObj.length;
+
+//     // 无参
+//     if (argLen == 0) {
+//         return len;
+//     }
+
+//     var sArr = new Array(len);
+
+//     // 备份
+//     for (var i = 0; i < len; i++) {
+//         if (!hasOwnProperty.call(O, i)) {
+//             continue;
+//         }
+//         sArr[i] = O[i];
+//     }
+
+//     // 头部添加
+//     for (var i = 0; i < argLen; i++) {
+//         if (!hasOwnProperty.call(argsObj, i)) {
+//             delete O[i];
+//             continue;
+//         }
+//         O[i] = argsObj[i];
+//     }
+
+//     // 复制备份的数组
+//     for (var i = 0; i < len; i++) {
+//         if (!hasOwnProperty.call(sArr, i)) {
+//             delete O[i+ argLen];
+//             continue;
+//         }
+//         O[i + argLen] = sArr[i];
+//     }
+
+//     O.length = len + argLen;
+//     return len + argLen;
+
+// }
+
+
 Array.prototype.unshift = function () {
 
     var O = Object(this);
@@ -15,15 +66,19 @@ Array.prototype.unshift = function () {
         return len;
     }
 
-    var sArr = new Array(len);
-
-    // 复制源数组
-    for (var i = 0; i < len; i++) {
+    // 先位移原数据, 倒叙
+    var tIndex;
+    for (var i = len - 1; i >= 0; i--) {
+        tIndex = argLen + i;
         if (!hasOwnProperty.call(O, i)) {
+            O[tIndex] = undefined;
+            delete O[tIndex]
             continue;
         }
-        sArr[i] = O[i];
+        O[tIndex] = O[i];
     }
+
+    console.log("::", O);
 
     // 头部添加
     for (var i = 0; i < argLen; i++) {
@@ -34,23 +89,13 @@ Array.prototype.unshift = function () {
         O[i] = argsObj[i];
     }
 
-    for (var i = 0; i < len; i++) {
-        if (!hasOwnProperty.call(sArr, i)) {
-            delete O[i+ argLen];
-            continue;
-        }
-        O[i + argLen] = sArr[i];
-    }
-
     O.length = len + argLen;
     return len + argLen;
-
 
 }
 
 
 
+var a = [1, 2, 3].concat(new Array(1));  //  [ 1, 2, 3, <1 empty item>]
 
-var a = [1, 2, 3].concat(new Array(1));
-
-console.log(a.unshift(7, 8), a)
+console.log(a.unshift(7, 8), a)   // 6 [ 7, 8, 1, 2, 3, <1 empty item> ]
