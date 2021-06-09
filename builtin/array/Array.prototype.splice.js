257a1ead8924c2af205e9c0aa1e2e9dbcaa2b360
TKOFFICE\huxw <huxw@mail.jj.cn>
2021-06-09 12:42:48 +0800

    实现splice

--- builtin/array/Array.prototype.splice.js
+++ builtin/array/Array.prototype.splice.js
@@ -28,12 +28,14 @@ Array.prototype.splice = function (start, deleteCount) {
     var O = Object(this);
     var len = O.length;
     var args = arguments;
-    var ret = [];
 
     var st = getNumber(start, 0);
     if (st < 0) {
         st = Math.max(0, len + st);
     }
+    st = st < 0 ? 0 : st;
+    st = st > len - 1 ? len - 1 : st;
+
     var dCount = getNumber(deleteCount, 0);
     dCount = dCount < 0 ? 0 : Math.min(dCount, len - st)
 
@@ -42,38 +44,37 @@ Array.prototype.splice = function (start, deleteCount) {
     var gap = addCount - dCount;
     var newLen = len - dCount + addCount;
 
+    var ret = new Array(dCount);
 
     if (st <= len && st >= 0) {
 
-        var retLen = ret.length;
+        var retIndex = 0;
 
         // 删得比加的多，前移
         if (gap < 0) {
+            // 顺序移动
             for (var i = st; i < len; i++) {
-
                 if (i < st + deleteCount) {
-                    ret[retLen] = O[i];
-                    retLen++;
+                    ret[retIndex] = O[i];
+                    retIndex++;
                 } else {
                     O[i + gap] = O[i]
                     delete O[i];
                 }
             }
         } else {  // 删的比加的少，整体后移动   
-
             // 倒叙移动
             for (var i = newLen; i >= st; i--) {
-
-                if (i < st + deleteCount) {
-                    ret[retLen] = O[i];
-                    retLen++;
+                if (i < st + deleteCount) {                    
+                    ret[deleteCount - retIndex - 1] = O[i];
+                    retIndex++
                 } else {
                     O[i + gap] = O[i]
                 }
             }
         }
     }
-    
+
     O.length = newLen;
 
     // 添加新元素
@@ -91,11 +92,16 @@ Array.prototype.splice = function (start, deleteCount) {
 
 var a = [1, 2, 3, 4];
 
-console.log(a.splice(-100, 2, 9, 8, 7), a);
+console.log(a.splice(-100, 2, 9, 8, 7), a, a.length);  // [ 1, 2 ] [ 9, 8, 7, 3, 4 ] 5
 
-    // var a = [1, 2, 3, 4];
+var a = [1, 2, 3, 4];
 
-    // console.log(a.splice(-2, 2, new Array(1), ...[new Array()]), a);   // [ 3, 4 ] [ 1, 2, [ <1 empty item> ], [] ]
+console.log(a.splice(-2, 2, new Array(1), ...[new Array()]), a, a.length);   // [ 3, 4 ] [ 1, 2, [ <1 empty item> ], [] ] 4
 
-    // var a = [1, 2, 3, 4];
-    // console.log(a.splice(5, 2, new Array(1), ...[new Array()]), a);  // [] [ 1, 2, 3, 4, [ <1 empty item> ], [] ]
+var a = [1, 2, 3, 4];
+console.log(a.splice(5, 2, new Array(1), ...[new Array()]), a, a.length);  // [] [ 1, 2, 3, 4, [ <1 empty item> ], [] ] 6
+
+
+var myFish = ['parrot', 'anemone', 'blue', 'trumpet', 'sturgeon'];
+var removed = myFish.splice(myFish.length - 3, 2);
+console.log(removed, myFish, myFish.length); // [ 'blue', 'trumpet' ] [ 'parrot', 'anemone', 'sturgeon' ] 3
